10a23f691c39c2d295177c896b3d054b83c260c3
rue_a <arne.ruemmler@gmail.com>
2021-09-15 18:10:39 +0200

    read and write highscore

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 app.listen(3000, () => console.log('listening at port 3000'));
+
+
+// read and write files in node tutorial:
+// https://stackabuse.com/reading-and-writing-json-files-with-node-js/
+function readJSON(path) {
+    let data = fs.readFileSync(path);
+    let highscore = JSON.parse(data);
+    return highscore;
+}
+
+function writeJSON(path, json_string) {
+    fs.writeFileSync(path, json_string);
+}
+
+// highscore = readJSON('private/highscore.json')
+// highscore['microsoft sam'] = 999;
+// highscore['jutta'] = 54
+// writeJSON('private/highscore.json', JSON.stringify(highscore))
+
+
+
 
 app.use(express.static('public'));
 
